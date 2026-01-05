68e6950b1c4e373417c448cefa09f44a1c492a33
Momiji142857 <159776672+Momiji142857@users.noreply.github.com>
2026-01-05 20:42:50 +0800

    修改了VeSchematics文件

--- src/VeSchematics/VeSchematics.java
+++ src/VeSchematics/VeSchematics.java
@@ -1,4 +1,32 @@
 package VeSchematics;
 
-public class VeSchematics {
+import arc.Core;
+import arc.Events;
+import arc.util.Log;
+import arc.util.Time;
+import mindustry.game.EventType;
+import mindustry.mod.Mod;
+import mindustry.ui.dialogs.BaseDialog;
+
+public class VeSchematics extends Mod {
+
+    public VeSchematics(){
+        Events.on(EventType.ClientLoadEvent.class, e -> {
+            //show dialog upon startup
+            Time.runTask(10f, () -> {
+                BaseDialog dialog = new BaseDialog("frog");
+                dialog.cont.add("behold").row();
+                //mod sprites are prefixed with the mod name (this mod is called 've-schematics' in its config)
+                dialog.cont.image(Core.atlas.find("ve-schematics-frog")).pad(20f).row();
+                dialog.cont.button("I see", dialog::hide).size(100f, 50f);
+                dialog.show();
+            });
+        });
+    }
+
+    @Override
+    public void loadContent(){
+        Log.info("Loading some example content.");
+    }
+
 }
